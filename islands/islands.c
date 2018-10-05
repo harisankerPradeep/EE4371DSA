e52eb49f07e3c7c90dc3d143115124e7e07351ca
harisankerPradeep <ep16b014@smail.iitm.ac.in>
2018-10-05 22:18:44 +0530

    islands with comments

--- islands/islands.c
+++ islands/islands.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <limits.h>
 struct neighbor{
 	struct vertex *value;
 	char *town;
@@ -14,6 +14,9 @@ struct vertex{
 	struct neighbor *start;
 	struct neighbor *end;
 	struct vertex *next;
+	int current_distance;
+	int set;
+	int set_distance;
 };
 int is = 0;
 
@@ -197,13 +200,149 @@ void split(struct vertex **h){
 	}
 
 }
+void push(int *heap,struct vertex **p,int w,struct vertex *v,int end,int cnt){
+	heap[end] = w;
+	p[end] = v;
+
+	int i = end;
+	while(i>0){
+		if(heap[i/2]>heap[i]){
+			int tmp = heap[i];
+			heap[i] = heap[i/2];
+			heap[i/2] = tmp;
+
+			struct vertex *t = p[i];
+			p[i] = p[i/2];
+			p[i/2] = t;
+			i = i/2;
+		}
+		else
+			break;
+
+	}
+
+	printf("pushing...\n");
+	for(i=0;i<end+1;i++)
+		printf("(%d %s)",heap[i],p[i]->town);
+	printf("\n");
+}
+
+struct vertex* extract(int *heap, struct vertex **p,int end,int cnt){
+	struct vertex *v = p[0];
+	v->set = 1;
+	v->set_distance = heap[0];
+	heap[0] = heap[end-1];
+	p[0] = p[end-1];
+
+	int i = 0;
+	while(i<(end-1)){
+		if(((2*i)+1)<(end-1) && heap[((2*i)+1)]<heap[i]){
+			int tmp = heap[i];
+			heap[i] = heap[((2*i)+1)];
+			heap[((2*i)+1)] = tmp;
+
+			struct vertex *t = p[i];
+			p[i] = p[((2*i)+1)];
+			p[((2*i)+1)] = t;
+			i = (2*i)+1;
+
+		}
+		else if(((2*i)+2)<(end-1) && heap[((2*i)+2)]<heap[i]){
+			int tmp = heap[i];
+			heap[i] = heap[(2*i)+2];
+			heap[(2*i)+2] = tmp;
+
+			struct vertex *t = p[i];
+			p[i] = p[(2*i)+2];
+			p[(2*i)+2] = t;
+			i = (2*i)+2;
+		}
+		else
+			break;
+	}
+	printf("extracting...");
+	for(i=0;i<end-1;i++)
+		printf("(%d %s)",heap[i],p[i]->town);
+	printf("\n");
+
+	return v;
+
+}
+void dijkstra(struct vertex **h,struct vertex **ve,int *min, struct vertex **cap){
+	struct vertex *head = *h;
+	struct vertex *v = *ve;
+//	printf("here! we do %s\n",v->town);
+	int cnt = 0;
+	while(head!=NULL){
+		if(head->island_number == v->island_number){
+			cnt++;
+			head->current_distance = INT_MAX;
+			head->set = 0;
+		}
+		head = head->next;	
+	}
+
+	int heap[cnt];
+	struct vertex **p = malloc(sizeof(struct vertex*)*cnt);
+	int end = 0;
+//	heap[0] = 0;
+//	p[0] = v;
+	v->set = 1;
+	v->set_distance = 0;
+
+	for(int i=0;i<cnt;i++){
+		int f = 0;
+		int d = v->set_distance;
+		struct neighbor *tmp = v->start;
+		while(tmp!=NULL){
+			if(tmp->value->set!=1 && (d+(tmp->weight))<tmp->value->current_distance){
+				push(heap,p,d+(tmp->weight),tmp->value,end,cnt);
+				end++;
+				f++;
+			}
+			tmp = tmp->next;
+		}
+		v = extract(heap,p,end,cnt);
+		end--;
+		
+	}		
+	head = *h;
+	int d = 0;
+	while(head!=NULL){
+		if(head->island_number == v->island_number){
+			printf("%s %d",head->town,head->set_distance);
+			d += head->set_distance;
+		}
+		head = head->next;	
+	}
+	v = *ve;
+//	printf("\nhere!  %s wit %d\n",v->town,d);
+	if(min[v->island_number-1]>d){
+		min[v->island_number-1] = d;
+		cap[v->island_number-1] = v;
+	}
+	if(min[v->island_number-1]==d){
+		if(strcmp(v->town,(cap[v->island_number-1])->town)<0){
+			min[v->island_number-1] = d;
+			cap[v->island_number-1] = v;
+		}
+	}
+}
+
+
+int comparator(const void *p, const void *q){ 
+	char *l = ((struct vertex *)p)->town; 
+	char *r = ((struct vertex *)q)->town;  
+	return strcmp(l,r); 
+} 
+
+
 int main(){
 	int v;
 	int e;
 	scanf("%d %d",&v,&e);
 
 	struct vertex *head = malloc(sizeof(struct vertex));
-	
 	for(int i=0;i<e;i++){
 		char *o = malloc(sizeof(char)*20);
 		char *d = malloc(sizeof(char)*20);
@@ -215,17 +354,32 @@ int main(){
 	
 	split(&head);
 	printf("here %s %s \n",head->town,head->next->end->value->town);		
-	while(head!=NULL){
-		printf("%s(%d) -->",head->town,head->island_number);
-		struct neighbor *tmp = head->start;
+//	while(head!=NULL){
+//		printf("%s(%d) -->",head->town,head->island_number);
+//		struct neighbor *tmp = head->start;
+//
+//		while(tmp!=NULL){
+//			printf("%s-->",tmp->town);
+//			tmp = tmp->next;
+//		}
+//		printf("\n");
+//		head = head->next;
+//	}
 
-		while(tmp!=NULL){
-			printf("%s-->",tmp->town);
-			tmp = tmp->next;
-		}
-		printf("\n");
-		head = head->next;
+	int min_distance[is];
+	for(int i=0;i<is;i++)
+		min_distance[i] = INT_MAX;
+	struct vertex **capital = malloc(sizeof(struct vertex*)*is);
+	struct vertex *tmp = head;
+	while(tmp!=NULL){
+		dijkstra(&head,&tmp,min_distance,capital);
+		tmp = tmp->next;
 	}
 
+	int size = sizeof(capital) / sizeof(capital[0]); 
+	qsort((void*)capital, is, sizeof(capital[0]), comparator); 
+	printf("Result \n");
+	for(int i=0;i<is;i++)
+		printf("%s\n",(capital[i])->town);	
 	return 0;
 }
