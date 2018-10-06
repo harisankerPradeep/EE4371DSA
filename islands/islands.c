ec496bbd4fc0415ac8a25aabc9d5b1fccbcffe12
harisankerPradeep <ep16b014@smail.iitm.ac.in>
2018-10-06 16:55:21 +0530

    everything except sorting?

--- islands/islands.c
+++ islands/islands.c
@@ -17,6 +17,7 @@ struct vertex{
 	int current_distance;
 	int set;
 	int set_distance;
+	int added_in_heap;
 };
 int is = 0;
 
@@ -26,7 +27,6 @@ void insert(struct vertex **h,char *o,char *d, int w){
 	if(head->town==NULL){
 		head->town = malloc(strlen(o)+1);
 		strcpy(head->town,o);
-		head->island_number = ++is;
 
 		struct neighbor *n = malloc(sizeof(struct neighbor));
 		n->town = malloc(strlen(d)+1);
@@ -38,7 +38,6 @@ void insert(struct vertex **h,char *o,char *d, int w){
 		struct vertex *ne = malloc(sizeof(struct vertex));
 		ne->town = malloc(strlen(d)+1);
 		strcpy(ne->town,d);
-		ne->island_number = is;
 
 		n = malloc(sizeof(struct neighbor));
 		n->town = malloc(strlen(o)+1);
@@ -177,28 +176,16 @@ void insert(struct vertex **h,char *o,char *d, int w){
 
 }
 
-void split(struct vertex **h){
-	struct vertex *head = *h;
-	head->island_number = ++is;
-	head = head->next;
-	while(head!=NULL){
-		struct neighbor *tmp = head->start;
-		int f = 0;
-		while(tmp!=NULL){
-			if(tmp->value->island_number!=0){
-				head->island_number = tmp->value->island_number;
-				f++;
-				break;
-			}
-			tmp = tmp->next;
-		}
-
-		if(f==0){
-			head->island_number = ++is;
-		}
-		head = head->next;
+void set_isn(struct vertex *h,int isn){
+	if(h->island_number!=0)
+		return;
+	h->island_number = isn;
+//	printf("setting %s to %d\n",h->town,h->island_number);
+	struct neighbor *tmp = h->start;
+	while(tmp!=NULL){
+		set_isn(tmp->value,isn);
+		tmp = tmp->next;
 	}
-
 }
 void push(int *heap,struct vertex **p,int w,struct vertex *v,int end,int cnt){
 	heap[end] = w;
@@ -221,12 +208,41 @@ void push(int *heap,struct vertex **p,int w,struct vertex *v,int end,int cnt){
 
 	}
 
-//	printf("pushing...\n");
-//	for(i=0;i<end+1;i++)
-//		printf("(%d %s)",heap[i],p[i]->town);
-//	printf("\n");
+	printf("pushing... %s \n",v->town);
+	for(i=0;i<end+1;i++)
+		printf("(%d %s)",heap[i],p[i]->town);
+	printf("\n");
 }
+void replace(int *heap,struct vertex **p,int w,struct vertex *v,int end,int cnt){
+	int i = 0;
+	for(i=0;i<end;i++)
+		if(strcmp(p[i]->town,v->town)==0)
+			break;
 
+	heap[i] = w;
+	p[i] = v;
+
+	while(i>0){
+		if(heap[i/2]>heap[i]){
+			int tmp = heap[i];
+			heap[i] = heap[i/2];
+			heap[i/2] = tmp;
+
+			struct vertex *t = p[i];
+			p[i] = p[i/2];
+			p[i/2] = t;
+			i = i/2;
+		}
+		else
+			break;
+
+	}
+
+	printf("replacing... %s \n",v->town);
+	for(i=0;i<end;i++)
+		printf("(%d %s)",heap[i],p[i]->town);
+	printf("\n");
+}
 struct vertex* extract(int *heap, struct vertex **p,int end,int cnt){
 	struct vertex *v = p[0];
 	v->set = 1;
@@ -236,7 +252,30 @@ struct vertex* extract(int *heap, struct vertex **p,int end,int cnt){
 
 	int i = 0;
 	while(i<(end-1)){
-		if(((2*i)+1)<(end-1) && heap[((2*i)+1)]<heap[i]){
+		if(((2*i)+1)<(end-1) && heap[((2*i)+1)]<heap[i]&&((2*i)+2)<(end-1)&& heap[((2*i)+2)]<heap[i]){
+			if(heap[(2*i)+1]<heap[(2*i)+2]){
+				int tmp = heap[i];
+				heap[i] = heap[((2*i)+1)];
+				heap[((2*i)+1)] = tmp;
+
+				struct vertex *t = p[i];
+				p[i] = p[((2*i)+1)];
+				p[((2*i)+1)] = t;
+				i = (2*i)+1;
+			}
+			else{
+				int tmp = heap[i];
+				heap[i] = heap[(2*i)+2];
+				heap[(2*i)+2] = tmp;
+
+				struct vertex *t = p[i];
+				p[i] = p[(2*i)+2];
+				p[(2*i)+2] = t;
+				i = (2*i)+2;
+			}
+
+		}
+		else if(((2*i)+1)<(end-1) && heap[((2*i)+1)]<heap[i]){
 			int tmp = heap[i];
 			heap[i] = heap[((2*i)+1)];
 			heap[((2*i)+1)] = tmp;
@@ -260,10 +299,12 @@ struct vertex* extract(int *heap, struct vertex **p,int end,int cnt){
 		else
 			break;
 	}
-//	printf("extracting...");
-//	for(i=0;i<end-1;i++)
-//		printf("(%d %s)",heap[i],p[i]->town);
-//	printf("\n");
+
+
+	printf("extracting.. (%s,%d) \n",v->town,v->set_distance);
+	for(i=0;i<end;i++)
+		printf("(%d %s)",heap[i],p[i]->town);
+	printf("\n");
 
 	return v;
 
@@ -280,7 +321,9 @@ int dijkstra(struct vertex **h,struct vertex **ve){
 		if(head->island_number == v->island_number){
 			cnt++;
 			head->current_distance = INT_MAX;
+			head->set_distance = INT_MAX;
 			head->set = 0;
+			head->added_in_heap = 0;
 		}
 		head = head->next;	
 	}
@@ -293,22 +336,29 @@ int dijkstra(struct vertex **h,struct vertex **ve){
 	v->set = 1;
 	v->set_distance = 0;
 	
-	for(int i=0;i<cnt;i++){
+	for(int i=0;i<cnt-1;i++){
 		int f = 0;
 		int d = v->set_distance;
 		struct neighbor *tmp = v->start;
 		while(tmp!=NULL){
 			if(tmp->value->set!=1 && (d+(tmp->weight))<tmp->value->current_distance){
-				push(heap,p,d+(tmp->weight),tmp->value,end,cnt);
-				end++;
-				f++;
+				if(tmp->value->added_in_heap==0){
+					push(heap,p,d+(tmp->weight),tmp->value,end,cnt);
+					end++;
+					f++;
+					tmp->value->added_in_heap=1;
+					tmp->value->current_distance = d+(tmp->weight);
+				}
+				else{
+					printf(" %d is replacing %d\n",d+(tmp->weight),tmp->value->current_distance);
+					replace(heap,p,d+(tmp->weight),tmp->value,end,cnt);
+				}
 			}
 			tmp = tmp->next;
 		}
 		v = extract(heap,p,end,cnt);
 		end--;
 	}		
-
 
 	head = tmph;
 	int d = 0;
@@ -327,8 +377,8 @@ int dijkstra(struct vertex **h,struct vertex **ve){
 
 
 int comparator(const void *p, const void *q){ 
-	char *l = ((struct vertex *)p)->town; 
-	char *r = ((struct vertex *)q)->town;  
+	const char *l =  *(const char**)p;
+	const char *r = *(const char**)q;  
 	return strcmp(l,r); 
 } 
 
@@ -339,6 +389,7 @@ int main(){
 	scanf("%d %d",&v,&e);
 
 	struct vertex *head = malloc(sizeof(struct vertex));
+	struct vertex *tmph = head;
 	for(int i=0;i<e;i++){
 		char *o = malloc(sizeof(char)*20);
 		char *d = malloc(sizeof(char)*20);
@@ -348,22 +399,30 @@ int main(){
 	}
 	is = 0;
 	
-	split(&head);
-//	while(head!=NULL){
-//		printf("%s(%d) -->",head->town,head->island_number);
-//		struct neighbor *tmp = head->start;
-//
-//		while(tmp!=NULL){
-//			printf("%s-->",tmp->town);
-//			tmp = tmp->next;
-//		}
-//		printf("\n");
-//		head = head->next;
-//	}
+	
+	while(head!=NULL){
+//		printf("set_isn of %s \n",head->town);
+		if(head->island_number==0){
+			set_isn(head,++is);
+		}
+		head = head->next;
+	}
+	head = tmph;
+	while(head!=NULL){
+		printf("%s(%d) -->",head->town,head->island_number);
+		struct neighbor *tmp = head->start;
 
+		while(tmp!=NULL){
+			printf("%s-%d->",tmp->town,tmp->weight);
+			tmp = tmp->next;
+		}
+		printf("\n");
+		head = head->next;
+	}
+	head = tmph;
 	int min_distance[is];
 	for(int i=0;i<is;i++)
-		min_distance[i] = INT_MAX-10;
+		min_distance[i] = INT_MAX;
 	struct vertex **capital = malloc(sizeof(struct vertex*)*is);
 	struct vertex *tmp = head;
 	while(tmp!=NULL){
@@ -371,23 +430,36 @@ int main(){
 		for(int i=0;i<is;i++)
 			printf("%d-->",min_distance[i]);
 		int d = dijkstra(&head,&tmp);
+		printf("finished dijkstras of %s isn %d  with %d\n",tmp->town,tmp->island_number,d);
+		printf("new calculated = %d; existing =%d \n",d,min_distance[(tmp->island_number)-1]);
 		if(min_distance[(tmp->island_number)-1]>d){
 			min_distance[tmp->island_number-1] = d;
 			capital[tmp->island_number-1] = tmp;
+			printf("capital replacement of island %d with %s",tmp->island_number,tmp->town);
 		}
 		else if(min_distance[tmp->island_number-1]==d){
 			if(strcmp(tmp->town,(capital[tmp->island_number-1])->town)<0){
 				min_distance[tmp->island_number-1] = d;
 				capital[tmp->island_number-1] = tmp;
+				printf("capital replacement of island %d with %s",tmp->island_number,tmp->town);
 			}
 		}
 		tmp = tmp->next;
 	}
+	printf("Result \n");
+	char **capital_names = malloc(sizeof(char)*is);
+
+	for(int i=0;i<is;i++){
+		capital_names[i] = malloc(sizeof(char)*20);
+		strcpy(capital_names[i],(capital[i])->town);
+		printf("%s\n",(capital_names[i]));	
+	}
 
 	int size = sizeof(capital) / sizeof(capital[0]); 
-	qsort((void*)capital, is, sizeof(capital[0]), comparator); 
+	printf("size of capitals %d,%d \n",size,is);
+	qsort((void*)capital_names, is, sizeof(capital_names[0]), comparator); 
 	printf("Result \n");
 	for(int i=0;i<is;i++)
-		printf("%s\n",(capital[i])->town);	
+		printf("%s\n",(capital_names[i]));	
 	return 0;
 }
